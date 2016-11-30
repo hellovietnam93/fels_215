49fd929f5b5eda83a2461691e9c128ebd57ade57
dieptran <dieptx.ptit@gmail.com>
2016-11-30 14:39:19 +0700

    Fix create edit category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,11 @@ class Category < ApplicationRecord
   has_many :lessons, dependent: :destroy
   has_many :words, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, length: {maximum: 20}
+  validates :duration, presence: true,
+    numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  validates :num_ques_per_lesson, presence: true,
+    numericality: {greater_than_or_equal_to: 0, only_integer: true}
 
   scope :sort, -> {order created_at: :desc}
 end
