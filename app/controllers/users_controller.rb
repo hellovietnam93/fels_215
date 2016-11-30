26cf0b8ef1fc11a64da367cfd53dd74b9b3a7836
Nguyen Thi An <nguyen.thi.an@framgia.com>
2016-11-30 15:35:36 +0700

    updating-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :find_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def index
   end
 
   def show
-    @user = User.find_by id: params[:id]
-    if @user.nil?
-      flash[:danger] = "User not found"
-      redirect_to root_path
-    end
   end
 
   def new
@@ -24,9 +23,34 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "flash.update"
+      redirect_to @user
+    else
+      flash[:danger] = t "flash.error"
+      render :edit
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:danger] = "flash.error"
+      redirect_to root_url
+    end
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user.current_user? @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy" 
-  resources :users, except: [:edit, :update, :destroy]
+  delete "/logout", to: "sessions#destroy"
+  resources :users, except: [:destroy]
   namespace :admin do
     root "dashboard#index", as: :home
     resources :categories
